422b18905e84f631214f0239e8653c8ae1f4b719
PurplePizza123 <ian.mulcahy996@gmail.com>
2025-10-21 18:01:51 -0500

    added GateCommands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GateCommands.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import static org.firstinspires.ftc.teamcode.subsystems.Subsystems.flywheel;
+import static org.firstinspires.ftc.teamcode.subsystems.Subsystems.gate;
+
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+
+public class GateCommands {
+    public Command open() {
+        return complete(gate::open);
+    }
+
+    public Command close() {
+        return complete(gate::close);
+    }
+
+    private Command complete(Runnable runnable) {
+        return new InstantCommand(runnable, gate);
+    }
+}
