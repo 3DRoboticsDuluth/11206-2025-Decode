54e287f14786fcbde5d33aeb097c4c7b9a75a442
unknown <3drdProgramming>
2025-10-23 16:37:24 -0500

    addded deflectorCommands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DeflectorCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DeflectorCommands.java
@@ -1,5 +1,20 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import static org.firstinspires.ftc.teamcode.subsystems.Subsystems.deflector;
+
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+
 public class DeflectorCommands {
-    // TODO
+    public Command up() {
+        return complete(deflector::up);
+    }
+
+    public Command down() {
+        return complete(deflector::down);
+    }
+
+    private Command complete(Runnable runnable) {
+        return new InstantCommand(runnable, deflector);
+    }
 }
