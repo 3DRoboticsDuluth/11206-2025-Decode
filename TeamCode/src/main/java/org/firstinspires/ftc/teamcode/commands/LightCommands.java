482bce4d211596cc5ba4957f05cace2b46dfb04b
Ian <programming@3droboticsduluth.com>
2025-12-04 19:20:18 -0600

    Started working on light coding, need to figure out the prism...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LightCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LightCommands.java
@@ -1,5 +1,28 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import static org.firstinspires.ftc.teamcode.subsystems.Subsystems.light;
+
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+
 public class LightCommands {
-    // TODO
+    public Command zero() {
+        return complete(light::zero);
+    }
+
+    public Command one() {
+        return complete(light::one);
+    }
+
+    public Command two() {
+        return complete(light::two);
+    }
+
+    public Command three() {
+        return complete(light::three);
+    }
+
+    private Command complete(Runnable runnable) {
+        return new InstantCommand(runnable, light);
+    }
 }
