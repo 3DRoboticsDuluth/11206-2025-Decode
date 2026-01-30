193298655468a5b0a67d8a7edaadfc77f9742e2e
3drdProgramming <programming@3droboticsduluth.com>
2026-01-30 13:30:42 -0600

    kickstand tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/KickstandSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/KickstandSubsystem.java
@@ -6,22 +6,21 @@ import org.firstinspires.ftc.teamcode.adaptations.solverslib.ServoEx;
 
 @Configurable
 public class KickstandSubsystem extends HardwareSubsystem {
-    public static double MIN = 0.25;
-    public static double MAX = 0.95;
+    public static double LEFT_MIN = 0.25;
+    public static double LEFT_MAX = 0.95;
+    public static double RIGHT_MIN = 0.00;
+    public static double RIGHT_MAX = 0.70;
     public static double ENGAGE = 1;
     public static double DISENGAGE = 0;
     public static double POS = 0;
     public static boolean TEL = false;
 
-    // Left:  Engage: 0.96, Disengage: 0.19 -> Difference: 0.77
-    // Right: Engage: 0.30, Disengage: 1.00 -> Difference: 0.70
-
     public ServoEx servoLeft;
     public ServoEx servoRight;
 
     public KickstandSubsystem() {
-        servoLeft = getServo("kickstandLeft", MIN, MAX);
-        servoRight = getServo("kickstandRight", MIN, MAX, s -> s.setInverted(true));
+        servoLeft = getServo("kickstandLeft", LEFT_MIN, LEFT_MAX);
+        servoRight = getServo("kickstandRight", RIGHT_MIN, RIGHT_MAX, s -> s.setInverted(true));
     }
 
     @Override
