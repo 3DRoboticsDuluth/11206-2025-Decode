6151c060d1996f6d18b39dcd50ec501e55398b3a
Ian Mulcahy <3drdprogramming@3droboticsduluth.com>
2026-02-01 20:26:13 -0600

    Updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.controls.Controls;
 import org.firstinspires.ftc.teamcode.game.Alliance;
+import org.firstinspires.ftc.teamcode.game.Config;
 import org.firstinspires.ftc.teamcode.game.Side;
 
 @Autonomous(name = "Auto", preselectTeleOp = "Teleop")
@@ -14,13 +15,14 @@ import org.firstinspires.ftc.teamcode.game.Side;
 public class AutoOpMode extends OpMode {
     @Override
     public void initialize() {
-        super.initialize();
-
+        config = new Config();
         config.alliance = Alliance.UNKNOWN;
         config.side = Side.UNKNOWN;
         config.quanomous = null;
         config.delay = 0;
         config.robotCentric = false;
+
+        super.initialize();
 
         Controls.initializeAuto();
 
