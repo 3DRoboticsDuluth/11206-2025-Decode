149c2a2e0eba6f0fbca173748f970cda26463731
IanMulcahy <ian.mulcahy@3droboticsduluth.com>
2026-02-07 16:20:05 -0600

    update gate park to actually work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NavSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NavSubsystem.java
@@ -149,8 +149,8 @@ public class NavSubsystem {
     public Pose getParkingPose(boolean gate, Axial axial, Lateral lateral) {
         return createPose(
             gate ? 0 * TILE_WIDTH : (config.side == NORTH ? 1 * TILE_WIDTH: -2.5 * TILE_WIDTH),
-            gate ? 1.5 * -config.alliance.sign * TILE_WIDTH : 1 * TILE_WIDTH * -config.alliance.sign,
-            config.alliance.sign * 90,
+            gate ? 2 * -config.alliance.sign * TILE_WIDTH : 1 * TILE_WIDTH * -config.alliance.sign,
+            -config.alliance.sign * Math.toRadians(90),
             axial,
             lateral
         );
