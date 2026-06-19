144b5c234701fcd511af43a8e89f78df084dfa46
3drdProgramming <programming@3drobiticsduluth.com>
2026-06-19 04:02:21 -0500

    added gamepad one kickstand controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls/KickstandControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls/KickstandControls.java
@@ -4,10 +4,16 @@ import static com.seattlesolvers.solverslib.gamepad.GamepadKeys.Button.LEFT_BUMP
 import static com.seattlesolvers.solverslib.gamepad.GamepadKeys.Button.RIGHT_BUMPER;
 import static com.seattlesolvers.solverslib.gamepad.GamepadKeys.Button.START;
 import static org.firstinspires.ftc.teamcode.commands.Commands.kickstand;
+import static org.firstinspires.ftc.teamcode.opmodes.OpMode.gamepad1;
 import static org.firstinspires.ftc.teamcode.opmodes.OpMode.gamepad2;
 
 public class KickstandControls {
     public KickstandControls() {
+        gamepad1.getGamepadButton(START).negate()
+            .and(gamepad1.getGamepadButton(LEFT_BUMPER))
+            .and(gamepad1.getGamepadButton(RIGHT_BUMPER))
+            .toggleWhenActive(kickstand.engage(), kickstand.disengage());
+
         gamepad2.getGamepadButton(START).negate()
             .and(gamepad2.getGamepadButton(LEFT_BUMPER))
             .and(gamepad2.getGamepadButton(RIGHT_BUMPER))
